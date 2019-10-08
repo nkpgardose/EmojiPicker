cef93cd5fdc3059e0422f348c1527659464f9990
Neil Kim Gardose <nkpgardose@gmail.com>
2019-10-08 12:47:14 +1100

    Initial setup & styling for Emoji picker
    * index.css holds root css variables
    * dir management
    * Initial css for EmojiPicker. WIP on breaking this down.
    * Some html templates
    * .editorconfig & vscode

---
+++ src/components/EmojiPicker/index.js
@@ -0,0 +1,153 @@
+import React from "react";
+import "./../../index.css";
+import "./index.css";
+
+const propTypes = {};
+
+const defaultProps = {};
+
+function EmojiPicker() {
+  return (
+    <div className="EmojiPicker">
+      <div className="selection">
+        <div className="searchbar">
+          <div className="input"></div>
+          <div className="submit"></div>
+        </div>
+        <dl className="category">
+          <dt className="title">People</dt>
+          <dd className="collection">
+            <button className="item" aria-label="smile" role="img">
+              😀
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😃
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😄
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😀
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😃
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😄
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😀
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😃
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😄
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😀
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😃
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😄
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😃
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😄
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+          </dd>
+          <dt className="title">Nature</dt>
+          <dd className="collection">
+            <button className="item" aria-label="smile" role="img">
+              😀
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😃
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😄
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😀
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😃
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😄
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😀
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😃
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😄
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😀
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😃
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😄
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😃
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😄
+            </button>
+            <button className="item" aria-label="smile" role="img">
+              😆
+            </button>
+          </dd>
+        </dl>
+      </div>
+    </div>
+  );
+}
+
+EmojiPicker.propTypes = propTypes;
+EmojiPicker.defaultProps = defaultProps;
+
+export default EmojiPicker;
