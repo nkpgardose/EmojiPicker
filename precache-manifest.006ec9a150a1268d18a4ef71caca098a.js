ceae2cf5c4bd163ce62f2ec2bdade1dd23de6f51
Neil Kim Gardose <nkpgardose@gmail.com>
2019-10-15 10:17:43 +0800

    Updates

---
+++ precache-manifest.006ec9a150a1268d18a4ef71caca098a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9604c70bca27c5aa0d",
+    "url": "build/2.9f9604c7.js"
+  },
+  {
+    "revision": "843c4f6972c709ee5716",
+    "url": "build/bundle.843c4f69.js"
+  },
+  {
+    "revision": "81bc79f7cf36fd248891",
+    "url": "build/main.81bc79f7.js"
+  },
+  {
+    "revision": "81ea89cbad037998afc445d1ce47ff83",
+    "url": "index.html"
+  },
+  {
+    "revision": "81bc79f7cf36fd248891",
+    "url": "static/css/main.c0e9a5c3.chunk.css"
+  }
+]);
