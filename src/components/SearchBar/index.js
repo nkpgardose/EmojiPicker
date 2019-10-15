01827d2ae0881299226c921919b7b51004ff4348
Neil Kim Gardose <nkpgardose@gmail.com>
2019-10-15 09:14:38 +0800

    💼 Add description on SearchBar's  propTypes

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -5,10 +5,18 @@ import "../../variables.css";
 import "./index.css";
 
 const propTypes = {
+  /** value reflects on SearchBar's input element. */
   input: PropTypes.string,
+  /** reference on a element for scrollTop condition. */
   emojiContent: PropTypes.shape({ current: PropTypes.object }).isRequired,
+  /**
+   * Returns the emojies search result when user is typing words on SearchBar.
+   * Typing `'hello'` on SearchBar will return `['wave']`.
+   * */
   onFieldSearch: PropTypes.func.isRequired,
+  /** new `value` being passed on input field when user enters a key. */
   setInput: PropTypes.func.isRequired,
+  /** new `results` being passed here. */
   setResults: PropTypes.func.isRequired
 };
 
