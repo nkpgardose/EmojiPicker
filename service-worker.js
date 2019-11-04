e80243cf24c4fd12f9357966d205b0dd5056d481
Neil Kim Gardose <nkpgardose@gmail.com>
2019-11-04 18:28:42 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nkpgardose/EmojiPicker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EmojiPicker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
